9ec464e5e61f64d64df0ce3b2b6a8770a1b959bb
Sukanyacse <68785445+Sukanyacse@users.noreply.github.com>
2021-07-10 21:20:23 +0530

    Add files via upload

---
+++ comparison operators.py
@@ -0,0 +1,11 @@
+#<, > ,<=, >=, ==, !=
+num1=100
+num2=500
+print(num1 < num2)
+print(num1 > num2)
+print(num1 <= num2)
+print(num1 >= num2)
+print(num1 == num2)
+print(num1 != num2)
+num3=100
+print(num1 == num3)
